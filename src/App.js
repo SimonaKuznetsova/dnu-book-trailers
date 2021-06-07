69bfc76cb5c6bf28410916b36e1e10e38ae1273b
Simona Kuznetsova <simonakuznecova@gmail.com>
2021-06-07 19:40:46 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Helloooo Liza Belmas! Ne mochiiii! Ne bom-bom i ne vyrchi</h1>
+      <p>а вы все козлы отстаньте и поставьте ей 100!!!!</p>
     </div>
   );
 }
